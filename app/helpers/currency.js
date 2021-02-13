833d5bd1472459db936e87d2a1ecde5969c822a5
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2021-02-13 23:43:13 +0300

    change custom helper (class variant)

--- app/helpers/currency.js
+++ app/helpers/currency.js
@@ -1,17 +1,19 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 
-export default helper(function currency(params, hash = {}) {
-  // {{currency 25}}
-  const [number] = params;
-  const {
-    sign = '$'
-  } = hash;
-  const dollars = Math.floor(number);
-  let cents = Math.floor(number * 100 % 100);
+export default class currency extends Helper {
+    compute(params, hash = {}) {
+        // {{currency 25}}
+        const [number] = params;
+        const {
+            sign = '$'
+        } = hash;
+        const dollars = Math.floor(number);
+        let cents = Math.floor(number * 100 % 100);
 
-  if (cents.toString().length === 1) {
-    cents = '0' + cents;
-  }
+        if (cents.toString().length === 1) {
+            cents = '0' + cents;
+        }
 
-  return `${sign}${dollars}.${cents}`;
-});
+        return `${sign}${dollars}.${cents}`;
+    }
+};
