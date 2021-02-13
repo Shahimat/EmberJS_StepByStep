fe448a6c6714a3547e338b16b942e907bd0edc6e
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2021-02-13 23:09:51 +0300

    added helpers

--- app/components/product.js
+++ app/components/product.js
@@ -1,10 +1,5 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 export default class ProductComponent extends Component {
-    @action
-    onShow() {
-        console.log(this);
-        alert(`DAAAA!`);
-    }
+    productImage = this.args.product.colors[0].image
 }
