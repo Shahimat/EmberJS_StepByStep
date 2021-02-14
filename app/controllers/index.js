d5f656d81f3a6d8b42a00e460cf534caf38232fc
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2021-02-14 04:42:12 +0300

    ember data transfom

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,20 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class IndexController extends Controller {
+    @tracked text = 'Нажми на меня';
+    index = 0;
 
     @action
     async getData() {
-        console.log(this.model);
         const response = await fetch('http://0.0.0.0:8080/http://localhost:3000');
         const { data } = await response.json();
-        alert(data);
+        this.text = data[this.index].name;
+        this.index = this.index + 1;
+        if (this.index > 1) {
+            this.index = 0;
+        }
     }
 
 }
